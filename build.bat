7609042e317340d58b1df918e75d268f0a0e9c33
SpencasaurusRex <SpencasaurusRex@gmail.com>
2020-01-27 04:00:02 -0500

    Remove mixed content

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 pushd "../spencasaurusrex.github.io" || exit /B 1
 for /D %%D in ("*") do (
     if /I not "%%~nxD"==".git" rd /S /Q "%%~D"
@@ -6,5 +7,4 @@ for %%F in ("*") do (
     del "%%~F"
 )
 popd
-
 bundle exec jekyll build JEKYLL_ENV=production & XCOPY "./_site" "../spencasaurusrex.github.io" /E
